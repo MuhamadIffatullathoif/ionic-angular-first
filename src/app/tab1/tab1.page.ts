f107062ad7603edf7d1c44f21abb9b7774a65311
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-01 13:59:36 +0700

    03. Debug an ionic application - debugger

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+    let number = 10;
+    // debugger;
+    number = 20;
+    number = number + 10;
+    number = 0;
+    console.log({number})
+  }
 
 }
